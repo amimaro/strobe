0c68738041933b7f9995f41990bf520bb3aeb5c9
Amimaro <amir.zahlan@gmail.com>
2018-03-30 11:42:36 -0300

    Create canvas :art:

---
+++ src/components/Canvas/Canvas.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Canvas.css';
+
+class Canvas extends Component {
+  render() {
+    return (
+      <div className="Canvas">
+
+      </div>
+    );
+  }
+}
+
+export default Canvas;
