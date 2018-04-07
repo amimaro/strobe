adb0cff08fa6c7cfedbbb6e1dd94b8e0de2502b2
Amimaro <amir.zahlan@gmail.com>
2018-04-07 11:13:13 -0300

    Improve menu :art:

--- src/components/Menu/MenuHeader/MenuHeader.js
+++ src/components/Menu/MenuHeader/MenuHeader.js
@@ -4,9 +4,6 @@ import './MenuHeader.css';
 class MenuHeader extends Component {
   render() {
     return (<div className="MenuHeader">
-      <center>
-        <h1>Menu</h1>
-      </center>
     </div>);
   }
 }
