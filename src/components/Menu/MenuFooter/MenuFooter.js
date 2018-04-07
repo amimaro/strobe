112e17a87a2b274dfea7e103f05fee9394e80b79
Amimaro <amir.zahlan@gmail.com>
2018-04-07 13:27:08 -0300

    Update MenuFooter :art:

--- src/components/Menu/MenuFooter/MenuFooter.js
+++ src/components/Menu/MenuFooter/MenuFooter.js
@@ -9,18 +9,16 @@ class MenuFooter extends Component {
       </center>
       <hr/>
       <center>
-        <p>
-          <a href="" target="_blank" rel="noopener noreferrer">
-            <b>strobe</b>
-          </a>. The source code is licensed under&nbsp;
-          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
-        </p>
-        <p>
-          &lt;/&gt; with
-          <span className="is-red">&nbsp;♥&nbsp;</span>
-          by&nbsp;
-          <a href="https://github.com/amimaro" target="_blank" rel="noopener noreferrer">amimaro</a>
-        </p>
+        <a href="" target="_blank" rel="noopener noreferrer">
+          <b>strobe</b>
+        </a>. The source code is licensed under&nbsp;
+        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+      </center>
+      <center>
+        &lt;/&gt; with
+        <span className="is-red">&nbsp;♥&nbsp;</span>
+        by&nbsp;
+        <a href="https://github.com/amimaro" target="_blank" rel="noopener noreferrer">amimaro</a>
       </center>
     </div>);
   }
