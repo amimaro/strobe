b75095c823aa6a79d02879d75e239b8696937a7e
Amimaro <amir.zahlan@gmail.com>
2018-05-01 07:30:09 -0300

    Create sound sense menu :art:

---
+++ src/components/Menu/MenuSound/MenuSound.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import './MenuSound.css';
+
+class MenuSound extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      soundSense: 1
+    }
+  }
+  render() {
+    return (<div className="MenuSound">
+      <button className={"btn default " + (
+          this.state.soundSense === 1
+          ? 'selected'
+          : '')} onClick={() => {
+          this.setState({
+            soundSense: 1
+          }, this.props.selectSoundSense(1))
+        }}>Range</button>
+      <button className={"btn default " + (
+          this.state.soundSense === 2
+          ? 'selected'
+          : '')} onClick={() => {
+          this.setState({
+            soundSense: 2
+          }, this.props.selectSoundSense(2))
+        }}>Beats</button>
+    </div>);
+  }
+}
+
+export default MenuSound;
